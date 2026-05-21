c808b8381ee8c8cf6d626fd9bd608967ca82e883
Ian Magarzo Fernandez <ian.magfer@gmail.com>
2026-05-21 22:33:24 +0200

    Initial project commit

---
+++ stack-overflow-users/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  stack-overflow-users
+//
+//  Created by Ian-Andoni Magarzo Fernandez on 21/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ stack-overflow-usersTests/stack_overflow_usersTests.swift
@@ -0,0 +1,18 @@
+//
+//  stack_overflow_usersTests.swift
+//  stack-overflow-usersTests
+//
+//  Created by Ian-Andoni Magarzo Fernandez on 21/5/26.
+//
+
+import Testing
+
+struct stack_overflow_usersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
