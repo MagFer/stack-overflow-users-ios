069b1b63936560e458f74f9c71be80ae6509aedf
Ian Magarzo Fernandez <ian.magfer@gmail.com>
2026-05-21 23:39:14 +0200

    Add users table
    * Configure a default `TableView` with 20 items to have a tangible UI.

--- stack-overflow-users/Scenes/UsersViewController.swift
+++ stack-overflow-users/Scenes/UsersViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 import SwiftUI
 
 final class UsersViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
         title = "Users"
+
+        applyStyling()
+        configureTableView()
+    }
+
+    private func applyStyling() {
+        view.backgroundColor = .red
+    }
+
+    private func configureTableView() {
+        self.addTableView()
+        self.registerCells()
+    }
+    
+    private func addTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+    private func registerCells() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "UserCell")
+    }
+}
+
+extension UsersViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath)
+        
+        var content  = cell.defaultContentConfiguration()
+        content.text = "Name \(indexPath.row+1)"
+        content.secondaryText = "User \(indexPath.row+1)"
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
 }
 
